5bcc001ce6da425e2029eab1df9a6f21ade16249
Antonio Dinkins <afdinki@gmail.com>
2020-06-28 21:31:45 -0400

    Added function declarations for endpoints

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,7 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res, next) => {
+    try {
 
+    } catch(err){
+        next(err)
+    }
+})
+
+router.get('/:id', (req, res, next)=> {
+    try {
+
+    } catch(err){
+        next(err)
+    }
+})
+
+router.post('/', (req, res, next)=> {
+    try {
+
+    } catch(err){
+        next(err)
+    }
+})
+
+module.exports = router
 
 
 module.exports = router
